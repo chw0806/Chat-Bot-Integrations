4c8bb89071b5697dc148b3fc41a07a8711539e1e
Cindy3184 <cindykrampah@yahoo.com>
2021-03-23 16:14:02 -0400

    added commands

--- server.js
+++ server.js
@@ -1,18 +1,58 @@
  //import discord library
-const Discord = require('discord.js');
+ const Discord = require('discord.js');
 
-//create discord client 
-const client = new Discord.Client(); 
+ //create discord client 
+ const client = new Discord.Client(); 
+ 
 
-//event listeners based on client 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-});
+ const prefix = "!";
 
-client.on('message', async msg => {
-    if(msg.content.startsWith('!Appointment')) {
-        msg.reply('What date would you like to schedule for?')
+
+ //event listeners based on client 
+ client.on('ready', () => {
+     console.log(`Logged in as ${client.user.tag}!`)
+ });
+ 
+ client.on("message", function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+  
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+  
+    if (command === "appointment") {
+      message.reply('What date would you like to schedule for? Reply with !date MM/DD/YYYY');
     }
-});
+    else if (command === "date") {
+        message.reply(`Your appointment is scheduled for ${command}`);
+    }
+  
+ });
+ 
+ client.login('ODIzNTUzMDg1MzQzMDA2NzYx.YFiflA.x7AIJekNmNb7jBqiM1vxb-DKCYI');
 
-client.login('ODIzNTUzMDg1MzQzMDA2NzYx.YFiflA.x7AIJekNmNb7jBqiM1vxb-DKCYI');
+
+
+
+
+
+
+
+
+// client.on('message', async msg => {
+    //     if (msg.author.bot) return;
+    //     if (!msg.content.startsWith(prefix)) return;
+      
+    //     const commandBody = msg.content.slice(prefix.length);
+    //     const args = commandBody.split(' ');
+    //     const command = args.shift().toLowerCase();
+    
+    //     if (command === "!appointment") {
+    //     msg.reply('What date would you like to schedule for? Reply with !date MM/DD/YYY');                   
+    //     }   
+    //     else if (command === "!date") {
+    //         msg.reply(`Your appointment is scheduled for ${commandBody}`);                      
+    //         }   
+    //   });
+
