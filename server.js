aee11a7be789a46a49b3b3292430f7568fbfe524
Cindy3184 <cindykrampah@yahoo.com>
2021-03-28 11:53:48 -0400

    gitignore

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+
  //import discord library
  const Discord = require('discord.js');
+
 
  //create discord client 
  const client = new Discord.Client(); 
  
+
 
  const prefix = "!";
 
@@ -27,32 +30,6 @@
     else if (command === "date") {
         message.reply(`Your appointment is scheduled for ${args}`);
     }
-  
  });
  
  client.login('ODIzNTUzMDg1MzQzMDA2NzYx.YFiflA.x7AIJekNmNb7jBqiM1vxb-DKCYI');
-
-
-
-
-
-
-
-
-
-// client.on('message', async msg => {
-    //     if (msg.author.bot) return;
-    //     if (!msg.content.startsWith(prefix)) return;
-      
-    //     const commandBody = msg.content.slice(prefix.length);
-    //     const args = commandBody.split(' ');
-    //     const command = args.shift().toLowerCase();
-    
-    //     if (command === "!appointment") {
-    //     msg.reply('What date would you like to schedule for? Reply with !date MM/DD/YYY');                   
-    //     }   
-    //     else if (command === "!date") {
-    //         msg.reply(`Your appointment is scheduled for ${commandBody}`);                      
-    //         }   
-    //   });
-
